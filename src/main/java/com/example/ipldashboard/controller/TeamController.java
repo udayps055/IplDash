83ceab94e73596931cfaa18ea7fc1d22059ec874
UDAY PRATAP SINGH <udayps055@gmail.com>
2021-06-28 14:47:23 +0530

    Reactapp with team page

--- src/main/java/com/example/ipldashboard/controller/TeamController.java
+++ src/main/java/com/example/ipldashboard/controller/TeamController.java
@@ -4,11 +4,13 @@ import com.example.ipldashboard.model.Team;
 import com.example.ipldashboard.repository.MatchRepository;
 import com.example.ipldashboard.repository.TeamRepository;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin
 public class TeamController {
 
     private TeamRepository teamRepository;
