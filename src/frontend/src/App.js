d44e849aa586ec3e38940abbd560f8f8fc04684d
UDAY PRATAP SINGH <udayps055@gmail.com>
2021-06-29 15:42:17 +0530

    UI of teampage completed

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
 import { TeamPage } from './pages/TeamPage';
+import { MatchPage } from './pages/MatchPage';
+import {BrowserRouter, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <TeamPage />
+      <BrowserRouter>
+        <Switch>
+          <Route path = "/teams/:teamName/matches/:year">
+            <MatchPage />
+          </Route>
+          <Route path = "/teams/:teamName">
+            <TeamPage />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
